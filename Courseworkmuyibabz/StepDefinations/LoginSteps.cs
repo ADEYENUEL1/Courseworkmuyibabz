e8cb8d4b92a0918c8a2b5af8f614ab14713150e5
muyiiwa babatunde <46856879+muyibabz@users.noreply.github.com>
2019-06-10 04:33:51 +0100

    Create LoginSteps.cs

---
+++ Courseworkmuyibabz/StepDefinations/LoginSteps.cs
@@ -0,0 +1,55 @@
+﻿using Courseworkmuyibabz.PageObjects;
+using Courseworkmuyibabz.Utilities;
+using System;
+using TechTalk.SpecFlow;
+
+namespace Courseworkmuyibabz.StepDefinations
+{
+    [Binding]
+    public class LoginSteps : Hooks
+    {
+        LoginPage loginPage;
+
+        public LoginSteps()
+
+        {
+            loginPage = new LoginPage
+        }
+
+        [Given(@"I navigate to the giftrete")]
+        public void GivenINavigateToTheGiftrete()
+        {
+            Hooks.Driver.Navigate().GoToUrl("https://giftrete.com/");
+        }
+        
+        [When(@"I click on login link")]
+        public void WhenIClickOnLoginLink()
+        {
+            loginPage.clicklogin();
+        }
+        
+        [When(@"I enter username")]
+        public void WhenIEnterUsername()
+        {
+            loginPage.TypeEmail();
+        }
+        
+        [When(@"I enter password")]
+        public void WhenIEnterPassword()
+        {
+          loginPage.TypeEmail();
+        }
+        
+        [When(@"I click on login button")]
+        public void WhenIClickOnLoginButton()
+        {
+            loginPage.clickSignin();
+        }
+        
+        [Then(@"I am logged in to the site")]
+        public void ThenIAmLoggedInToTheSite()
+        {
+            FluentAssertions.
+        }
+    }
+}
